1819cd1675cd9f91cc5197a4cf879d779aa0e0b5
Jamie Seal <jamieseal@itg.co.uk>
2019-09-02 17:30:21 +0100

    wordpress-sp - more amends made

--- wordpress-sp/wp-content/themes/sealedperformance/home-page.php
+++ wordpress-sp/wp-content/themes/sealedperformance/home-page.php
@@ -2,17 +2,11 @@
 
     <?php get_header();?>
 
-    <div id="main">
-
         <div class="container">
 
             <!-- Banner -->
             <div class="banner"></div>
-        
-        <!-- End Main Container div -->
+
         </div>
-    
-    <!-- End Main ID div -->
-    </div>
 
     <?php get_footer();?>
