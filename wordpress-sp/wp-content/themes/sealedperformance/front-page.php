d24b5fd40cf50ffe7006f6454a437789e3fb6f18
Jamie Seal <jamieseal@itg.co.uk>
2019-09-10 17:22:05 +0100

    wordpress-sp - Quote added to front-page

--- wordpress-sp/wp-content/themes/sealedperformance/front-page.php
+++ wordpress-sp/wp-content/themes/sealedperformance/front-page.php
@@ -1,17 +1,16 @@
     <?php get_template_part('layout');?>
     <?php get_header();?>
 
-    <div id="main">
-
         <div class="container">
+            
+            <section class="quote-container">
 
-            <!-- Quote -->
-            <div class="quote">What’s behind you doesn’t matter. — Enzo Ferrari</div>
+                <!-- Quote -->
+                <div class="quote">What’s <span class="quote-2">behind</span> you doesn’t <span class="quote-2">matter</span>. <br><span class="quote-3">— Enzo Ferrari</span></div>
+
+            </section>
         
         <!-- End Main Container div -->
         </div>
-    
-    <!-- End Main ID div -->
-    </div>
 
     <?php get_footer();?>
